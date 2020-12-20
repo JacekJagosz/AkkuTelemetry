f4519b4b7cb6e99706e198e8619430c38379035d
Jacek Jagosz <jacek.jagosz@outlook.com>
2020-12-20 21:33:59 +0100

    Import and show total voltages. Calculating energy from it to go.

--- mainPreparedForGUI.m
+++ mainPreparedForGUI.m
@@ -1,8 +1,10 @@
 clear
 clc
-filenamesToRead = ["_TEL_HVBMS_CURR.txt", "_TEL_HVBMS_MAXCVOLT.txt", "_TEL_HVBMS_MINCVOLT.txt"];
+filenamesToRead = ["_TEL_HVBMS_CURR.txt", "_TEL_HVBMS_MAXCVOLT.txt", "_TEL_HVBMS_MINCVOLT.txt", "_TEL_HVBMS_VOLT.txt"];
 sFolderPath='/home/jacek/Dokumenty/Matlab/Testy LEM MotoPark/lem_logi_motopark_30102020/20201030_092123662678_decoded';
 %sFolderPath='/home/jacek/Dokumenty/Matlab/Testy LEM MotoPark/lem_logi_motopark_30102020/20201030_100139001723_decoded';
 [oTimestamps, times, val] = readSensorsInFolder(filenamesToRead, sFolderPath);
 length(oTimestamps)
-plot(times, val(:,1))
+plot(times, val(:,2:3))
+hold on
+plot(times, val(:,4)+4, 'g')
