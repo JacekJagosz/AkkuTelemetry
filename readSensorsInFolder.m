6dd0e1fca2c3a4fa180d61d747acad9973c633a8
Jacek Jagosz <jacek.jagosz@outlook.com>
2022-11-11 22:52:28 +0100

    Add filtering for current peaks, problematic with our new BMS

--- readSensorsInFolder.m
+++ readSensorsInFolder.m
@@ -1,7 +1,7 @@
 function [oTimestamp, times, v]=readSensorsInFolder(filenamesToRead, sFolderPath)
     %filenamesToRead = ["_TEL_HVBMS_CURR.txt", "_TEL_HVBMS_MAXCVOLT.txt", "_TEL_HVBMS_MINCVOLT.txt"];
     %sFolderPath='/home/jacek/Dokumenty/Matlab/Testy LEM MotoPark/lem_logi_motopark_30102020/20201030_100139001723_decoded';
-    firstFileName = erase(sFolderPath, '_decoded')
+    firstFileName = erase(sFolderPath, '_decoded');
     %get first part of folder name
     firstFileName = reverse(extractBetween(reverse(firstFileName), 1, filesep)) %filesep returns '/' on Linux and '\' on Windows
     %filenamesToRead %check if it was passed correctly
@@ -11,13 +11,22 @@ function [oTimestamp, times, v]=readSensorsInFolder(filenamesToRead, sFolderPath
         c=fscanf(fid_1, '%c');
         w=splitlines(c);
         w=split(w(1:(length(w)-1)));
-        v(:,i) = str2double((strrep(w(:, 2), ',','.')));
+        if i==1
+            v = zeros(length(w), length(filenamesToRead));
+        end
+        v(:,i) = str2double((strrep(w(:, 2), ',','.'))); %replace , with . because in some logs , was used as a decimal separator
         fclose(fid_1);
     end
-    times=zeros(1, length(w));
-    for i=1:length(w)
-        times(i) = cellfun(@(x)str2double(x),regexp(char(w(i,1)),':','split'))*[3600000;60000;1000;1;0.001];
-    end
-    
     oTimestamp=w(:, 1);
+    times=zeros(1, length(oTimestamp));
+    for i=1:length(oTimestamp)
+        times(i) = cellfun(@(x)str2double(x),regexp(char(oTimestamp(i)),':','split'))*[3600000;60000;1000;1;0.001];
+    end
+    %our latest BMS master sometimes sends garbagge CURR data. So let's at
+    %least delete clearly impossible data points
+    for i=1:length(oTimestamp)
+        if v(i,1) > 200
+            v(i,1) = v(i-1,1);
+        end
+    end
 end
